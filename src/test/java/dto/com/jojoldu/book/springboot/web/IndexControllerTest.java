703ab3767c1e7933266c9b211f6dd1df1d7c4c4a
esteban0829 <esteban0829@naver.com>
2022-03-23 17:34:49 +0900

    test: made test to check if message is inside main page

--- src/test/java/dto/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ src/test/java/dto/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -1,2 +1,30 @@
-package dto.com.jojoldu.book.springboot.web;public class IndexControllerTest {
+package dto.com.jojoldu.book.springboot.web;
+
+import com.jojoldu.book.springboot.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = Application.class)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void mainPageLoadTest() {
+        //when
+        String body = restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
 }
