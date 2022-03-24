88d63e8082f2109f868e29f023f20e441fcd0a45
esteban0829 <esteban0829@naver.com>
2022-03-24 16:09:50 +0900

    feat: listing posts added to main page

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,13 +1,24 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<PostsListResponseDto> postsList = postsService.findAllDesc();
+        model.addAttribute("posts", postsList);
         return "index";
     }
 
